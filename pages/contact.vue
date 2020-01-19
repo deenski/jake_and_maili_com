bdcd743d57dc623da61313db82e62c1f8478abed
deenski <jakevendegna@gmail.com>
2020-01-19 12:07:35 -0700

    feat(pages): added contact content

--- pages/contact.vue
+++ pages/contact.vue
@@ -1,12 +1,37 @@
 <template>
-  <construction />
+  <div class="container">
+    <div>
+      <h1 class="title">
+        🤍Jake & Maili Have A Wedding🤍
+      </h1>
+      <h2 class="subtitle">
+        📅: 10.15.20, 🗺️:
+        <a href="http://bit.ly/2tw5xa9" class="subtitle links">
+          River Garden Winery
+        </a>
+      </h2>
+      <p class="content">
+        <a href="mailto:jakeandmailiwedding@gmail.com" class="links">
+        Click here to RSVP
+        </a>
+        or email us directly at <span class="links">jakeandmailiwedding@gmail.com</span>
+        with a head count and what you're eating. If you're a Vegan, please
+        list that next to your name.
+      </p>
+      <p class="content">
+      Example message with the content we NEED, feel free to copy paste:
+      </p>
+      <div class="code-container">
+        <pre>
+          <code>
+          Attending:
+            Jakob - Paris Chicken
+            Maili - Vegan - Medeterainian Cous Cous
+          Not attending:
+            None
+          </code>
+        </pre>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-import Construction from '~/components/Construction.vue'
-export default {
-  components: {
-    Construction
-  }
-}
-</script>
