cebe5f431274a8c09f61b126b85f3d0f499a75e0
deenski <jakevendegna@gmail.com>
2020-01-19 12:06:56 -0700

    fix(links): explicit class names - gifts

--- pages/gifts.vue
+++ pages/gifts.vue
@@ -1,16 +1,29 @@
 <template>
   <div class="container">
     <div>
-      <h1 class="title">Gifts</h1>
+      <h1 class="title">
+        Gifts
+      </h1>
+      <h2 class="subtitle">
+        <a
+          href="https://paypal.me/pools/c/8ljrdscr74"
+          class="links"
+          target="_blank"
+        >
+          Honeymoon Trip to Japan
+        </a>
+      </h2>
       <p class="content">
         We are into experiences, as such we are asking for gifts in the form of
         donations to our travel fund. We are planning a trip to Japan. In
-        collegewe both took a class where we had to plan a trip.
-        Coincedentially, we both drew Japan out of a hat. So we have both
-        independently planned our own ideal trips to Japan. We will be combining
-        those plans as well as spending a few days with Jakob's sister Arielle
-        and her family, who recently moved to Ota-Shi Gunma Japan We appreciate
-        anything you can contribute to making this experience possible. Donate
+        college we both took a class where we had to plan a vacation.
+        Coincidentally, we both drew Japan out of a hat, and conducted quite a
+        lot of research. So we have both independently planned our own ideal
+        vacation to Japan. We can't imagine anything better than combining
+        those plans for a honeymoon. While we are checking off those boxes, we will
+        also be spending a few days with Jakob's sister Arielle
+        and her family, who recently moved to Ota-Shi Gunma, Japan. We appreciate
+        anything you can contribute to making this dream vacation a reality. Donate
         responsibly, and please don't feel like you have to give anything at
         all. Your presence is a present! If you would like to make a donation,
         follow this:
