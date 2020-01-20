b1efec2400e83ff9a8d054320a3a59ff9fb88298
deenski <jakevendegna@gmail.com>
2020-01-20 09:20:43 -0700

    fix(index): add disclaimer

--- pages/index.vue
+++ pages/index.vue
@@ -10,6 +10,14 @@
           River Garden Winery
         </a>
       </h2>
+      <h3 class="subtitle">
+        This site is largely 🛠️Under Construction🛠️.
+      </h3>
+      <p class="content">
+        We've tried to provide accurate information for you here. But, wedding
+        planning is no easy task. If you have any specific questions, feel free
+        to reach out and call either of us directly.
+      </p>
     </div>
   </div>
 </template>
