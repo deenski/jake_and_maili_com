f4fcc57c1ca5d4a435242cea8e7ca7afe0818b5a
deenski <jakevendegna@gmail.com>
2020-01-20 09:15:14 -0700

    feat(events): added page content

--- pages/events.vue
+++ pages/events.vue
@@ -1,12 +1,23 @@
 <template>
-  <construction />
+  <div class="container">
+    <div>
+      <h1 class="title">
+        🤍Jake & Maili Have A Wedding🤍
+      </h1>
+      <h2 class="subtitle">
+        The Timeline:
+      </h2>
+      <p class="content">
+        <ul style="list-style: none;">
+          <li>3:30-4pm Guests Arriving</li>
+          <li>4-4:30pm Ceremony</li>
+          <li>4:30-5:30pm Cocktail Hour</li>
+          <li>5:30-6:30pm Dinner</li>
+          <li>6:30-7pm First Dances</li>
+          <li>7-10pm 🎉 Party like it's 1999 🎉</li>
+        </ul>
+      </p>
+    </div>
+  </div>
 </template>
 
-<script>
-import Construction from '~/components/Construction.vue'
-export default {
-  components: {
-    Construction
-  }
-}
-</script>
