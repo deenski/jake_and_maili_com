c7258d6fa2b618740b89f07caaaf862ff0c38dba
Jakob Vendegna <deenski@users.noreply.github.com>
2021-02-13 14:11:09 -0700

    Update details.vue

--- pages/details.vue
+++ pages/details.vue
@@ -2,32 +2,14 @@
   <div class="container">
     <div class="card">
       <h1 class="title">
-        🤍Jake & Maili Have A Wedding🤍
+        🤍Jake & Maili Cancelled A Wedding🤍
       </h1>
       <h2 class="subtitle">
         The Details:
       </h2>
       <h3>
-        Dos and Don'ts:
+        Don't show up!
       </h3>
-      <p class="content left">
-        <ul style="list-style: none;">
-          <li>Do: <router-link to="/contact" target="self" class="links">RSVP</router-link> on time. (9/17/2020)</li>
-          <li>Do: Look up the weather forecast and dress appropriately: October, Outdoors, Colorado.</li>
-          <li>Do: Ensure you have a designated driver, Uber and Lyft aren't coming out this far.</li>
-          <li>Do: Arrange for <router-link to="/accomodations" target="self" class="links">accomodations</router-link> through us, or find your own.</li>
-          <li>Don't: Bring your RV or camping gear. All guests and their vehicles must have vacated the property by 11pm.</li>
-          <li>Don't: Litter. Pack it in Pack it out. Leave it cleaner than you found it. Be respectful of the property.</li>
-        </ul>
-      </p>
-      </br>
-      <p class="content">
-        4/27/2020: We have sent out our save the dates. If you know for a fact that you can't make it, we would truly
-        appreciate a heads-up. Declining early will allow us to send out a second round of invitations.
-      </p>
-      <p class="content">
-        6/14/2020: Changed the date, 7.14.2021 🖕🏽 Rona!
-      </p>
     </div>
   </div>
 </template>
